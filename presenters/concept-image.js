b048e65abe82c03b8abdd7957acf03df5931bfec
Dawn Budge <dawn.budge@ft.com>
2017-05-22 11:18:25 +0100

    Remove deprecated primaryImage

--- presenters/concept-image.js
+++ presenters/concept-image.js
@@ -1,18 +1,10 @@
 'use strict';
-const taxonomyImages = {
-	authors: 'http://com.ft.imagepublish.prod.s3.amazonaws.com/cca52406-bda0-11e5-9fdb-87b8d15baec2',
-	brand: 'http://com.ft.imagepublish.prod.s3.amazonaws.com/cca52406-bda0-11e5-9fdb-87b8d15baec2',
-	organisations: 'http://com.ft.imagepublish.prod.s3.amazonaws.com/cca52406-bda0-11e5-9fdb-87b8d15baec2',
-	people: 'http://com.ft.imagepublish.prod.s3.amazonaws.com/cca52406-bda0-11e5-9fdb-87b8d15baec2',
-	regions: 'http://com.ft.imagepublish.prod.s3.amazonaws.com/cca52406-bda0-11e5-9fdb-87b8d15baec2',
-	specialReports: 'http://com.ft.imagepublish.prod.s3.amazonaws.com/cca52406-bda0-11e5-9fdb-87b8d15baec2',
-	topics: 'http://com.ft.imagepublish.prod.s3.amazonaws.com/cca52406-bda0-11e5-9fdb-87b8d15baec2',
-	'default': 'http://com.ft.imagepublish.prod.s3.amazonaws.com/cca52406-bda0-11e5-9fdb-87b8d15baec2'
-};
+
+const fallbackImage = 'http://com.ft.imagepublish.prod.s3.amazonaws.com/cca52406-bda0-11e5-9fdb-87b8d15baec2';
 
 /**
-* @param {String} taxonomy name of concept type e.g. authors
 * @param {Object} items list of article items
+* @return {String} url of an image
 **/
 class ConceptImagePresenter {
 	constructor (data) {
@@ -20,18 +12,13 @@ class ConceptImagePresenter {
 	}
 	get imageUrl () {
 		if (!this.data || !this.data.items) {
-			return taxonomyImages.default;
+			return fallbackImage;
 		}
-		const findItemImage = this.data.items.find( item => {
-			return ((item.primaryImage && item.primaryImage.rawSrc)
-				|| (item.mainImage && (item.mainImage.url || item.mainImage.rawSrc)))
-				&& !item.isPodcast;
+		const findImageItem = this.data.items.find( item => {
+			return item.mainImage && item.mainImage.url && !item.isPodcast;
 		});
-		const conceptImage = findItemImage
-			&& ((findItemImage.primaryImage && findItemImage.primaryImage.rawSrc)
-			|| findItemImage.mainImage.url || findItemImage.mainImage.rawSrc);
-
-		return conceptImage || taxonomyImages[this.data.taxonomy] || taxonomyImages.default;
+		const conceptImage = findImageItem && findImageItem.mainImage.url;
+		return conceptImage || fallbackImage;
 	}
 }
 
