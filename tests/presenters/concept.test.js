b1c5992f09ebaadee03b49d75f015f5162db23b4
Ben Barnett <ben@brushfiredesign.net>
2017-11-16 13:05:42 +0000

    Clean up concept image  🐿 v2.5.16

--- tests/presenters/concept.test.js
+++ tests/presenters/concept.test.js
@@ -3,11 +3,6 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe('Concept Presenter', () => {
-	it('has a imageURL getter that returns a string', () => {
-		const inst = new ConceptPresenter({});
-		expect(typeof inst.imageUrl).to.equal('string');
-	});
-
 	it('has a responsiveGrids getter that returns a string', () => {
 		const inst = new ConceptPresenter({'show': {}});
 		expect(typeof inst.responsiveGrids).to.equal('string');
