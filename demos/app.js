bc8995e27985adf70f3e4cbda3c99794c070466a
umbobabo <umberto.babini@ft.com>
2022-01-21 11:59:50 +0000

    Resolve conflicts

--- demos/app.js
+++ demos/app.js
@@ -1,36 +1,53 @@
 'use strict';
 
-const conceptHelper = require('../handlebars-helpers/concept');
-const express = require('@financial-times/n-internal-tool');
-const handlebars = require('handlebars');
-const fixtures = require('./fixtures.json');
+const path = require('path');
+const fs = require('fs');
 const chalk = require('chalk');
+const handlebars = require('handlebars');
+const nExpress = require('@financial-times/n-express');
+const { PageKitHandlebars, helpers } = require('@financial-times/dotcom-server-handlebars');
+
+const fixtures = require('./fixtures.json');
+
 const errorHighlight = chalk.bold.red;
 const highlight = chalk.bold.green;
 
-handlebars.registerHelper('concept', conceptHelper);
-
-const app = module.exports = express({
+const app = module.exports = nExpress({
 	name: 'public',
 	systemCode: 'n-topic-card-demo',
 	withFlags: false,
-	withHandlebars: true,
-	withNavigation: false,
+	withConsent: false,
+	withServiceMetrics: false,
 	withAnonMiddleware: false,
-	hasHeadCss: false,
 	demo: true,
-	s3o: false,
-	viewsDirectory: '/demos',
-	layoutsDir: 'demos',
-	partialsDirectory: process.cwd(),
-	directory: process.cwd()
+	withBackendAuthentication: false,
 });
 
-app.get('/', (req, res) => {
-	res.render('demo', Object.assign({
-		title: 'Test App',
-		layout: 'demo-layout',
-	}, fixtures));
+const templateDirectory = path.join(__dirname, '../templates');
+
+fs.readdirSync(templateDirectory).forEach(filename => {
+	handlebars.registerPartial(
+		`n-topic-card/templates/${filename.substr(0, filename.lastIndexOf('.'))}`,
+		fs.readFileSync(path.join(templateDirectory, filename), 'utf8')
+	);
+});
+
+app.set('views', __dirname);
+app.set('view engine', '.html');
+
+app.engine('.html', new PageKitHandlebars({
+	cache: false,
+	handlebars,
+	helpers: {
+		...helpers,
+		concept: require('../handlebars-helpers/concept'),
+	}
+}).engine);
+
+app.use('/public', nExpress.static(path.join(__dirname, '../public'), { redirect: false }));
+
+app.get('/', (_, res) => {
+	res.render('index', fixtures);
 });
 
 function runPa11yTests () {
